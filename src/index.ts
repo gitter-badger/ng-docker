b75c4cc865cf9d6eb7f5c07e298249d9522b6024
terry.li <terry.li@dorry.io>
2016-10-15 22:23:49 +0800

    clear useless part & test travis

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,1 @@
-import {DIRECTIVES} from './directives';
-import {PIPES} from './pipes';
-import {PROVIDERS} from './services';
-
-export * from './directives';
-export * from './services';
-export * from './pipes';
-
-export default {
-  directives: [DIRECTIVES],
-  pipes: [PIPES],
-  providers: [PROVIDERS]
-}
+export { DockerModule } from './module/ng-docker.module'
